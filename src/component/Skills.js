3ac16db10692f1a02cdf98ce04102ee4c4266e17
Antarpreet <pnarain82@gmail.com>
2023-12-14 20:23:58 +1100

    tagcloud

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -2,19 +2,7 @@ import React from "react";
 function Skills() {
   return (
     <>
-      <div className="border border-radius-1">
-        <p>html</p>
-        <p>html</p>
-        <p>html</p>
-        <p>html</p>
-        <p>html</p>
-        <p>html</p>
-        <p>html</p>
-        <p>html</p>
-        <p>html</p>
-        <p>html</p>
-        <p>html</p>
-      </div>
+      <div></div>
     </>
   );
 }
