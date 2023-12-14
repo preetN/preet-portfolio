34d6c3a768990d23d9ff1a3d977c43c7c4841ffc
Antarpreet <pnarain82@gmail.com>
2023-12-14 12:11:55 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Contact from "./component/Contact";
+import Hero from "./component/Hero";
+import Projects from "./component/Projects";
+import Skills from "./component/Skills";
+import Footer from "./layout/Footer";
+import Navbar from "./layout/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
